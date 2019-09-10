f15a14395b3d8c485f035e96fd488e1d1628331a
Tam Muoi <you@example.com>
2019-09-10 23:20:38 +0700

    up

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,46 @@
 <footer class="footer">
-	<h1>FOOTER HERE</h1>
+	<div class="container">
+	
+			<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt consequuntur placeat earum odit officia obcaecati. Ex a, quae fuga vel laboriosam unde, voluptatum aperiam hic, quia tempore delectus dicta.</h1>
+		</div>
+	
 </footer>
