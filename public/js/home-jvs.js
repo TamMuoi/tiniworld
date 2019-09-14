d77f94c197e95dd4fd8746db9670e9efb7a00111
Tam Muoi <you@example.com>
2019-09-14 09:55:07 +0700

    up

--- public/js/home-jvs.js
+++ public/js/home-jvs.js
@@ -179,4 +179,31 @@ jQuery(document).ready(function($) {
       $('.header').removeClass('fix-area-top');
     }
   });
+
+
+
+
+  // SLIDE FEEDBACK
+  
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    PauseOnHover:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  })
 });
+
+
+
